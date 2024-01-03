2dbb2666a0aa92396bded2b3065a50fb9510fcc1
Guillaume Berche <guillaume.berche@orange.com>
2024-01-03 14:21:41 +0100

    create-release-branches.bash: force push existing branches

--- create-release-branches.bash
+++ create-release-branches.bash
@@ -10,6 +10,6 @@ for v in 1.25 1.26 1.27 1.28 1.29 1.30 1.31 ; do
   ! diff vendir.yml vendir.yml.orig
   git add vendir.yml
   git commit -m "set up release for version $v"
-  git push --set-upstream origin $BRANCH_NAME
+  git push --set-upstream origin $BRANCH_NAME --force
   git co master
 done
