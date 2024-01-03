1326618e1983b8ef3fc73bbbac006b063c3751fb
Guillaume Berche <guillaume.berche@orange.com>
2024-01-03 14:30:15 +0100

    create-release-branches.bash: remove vendir.yml.orig

--- create-release-branches.bash
+++ create-release-branches.bash
@@ -8,6 +8,7 @@ for v in 1.25 1.26 1.27 1.28 1.29 1.30 1.31 ; do
     git co $BRANCH_NAME
   sed -i.orig "s/tag: v.*/tag: v${v}.0/g" vendir.yml
   ! diff vendir.yml vendir.yml.orig
+  rm vendir.yml.orig
   git add vendir.yml
   git commit -m "set up release for version $v"
   git push --set-upstream origin $BRANCH_NAME --force
