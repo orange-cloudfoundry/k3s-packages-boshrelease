bee524ea0c935ff8e701d5f7ff8f7c7e17e4d3ef
Olivier ORAND <olivier.orand@orange.com>
2024-01-03 16:35:36 +0000

    chore(github-action): create PR for release-<x> branches (fixup)
    list existing PRs at the end

--- create-missing-PR.bash
+++ create-missing-PR.bash
@@ -12,5 +12,8 @@ for branch in $(git --no-pager branch -r|grep -E "/release-[0-9.]*$");do
   release=$(echo $branch|cut -d'/' -f2)
   branch_head_ref=$(echo $branch|sed "s@/@:@g")
   echo "gh pr create --base master -t \"$release\" -b \"We use this PR to track changes related to K8S $release\" -l \"target/$release\" -l k3s-base--head $release"
+  # We ignore PR creation errors
   gh pr create --base master -t "$release" -b "We use this PR to track changes related to K8S $release" -l "target/$release" -l k3s-base --head $release
 done
+
+gh pr list -l k3s-base
