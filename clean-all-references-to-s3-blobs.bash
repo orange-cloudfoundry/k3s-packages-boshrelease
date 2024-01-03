f29f0d392d6f12893021f0c2c8198d404defd86b
Guillaume Berche <guillaume.berche@orange.com>
2024-01-03 17:41:27 +0100

    refine clean-all-references-to-s3-blobs.bash

--- clean-all-references-to-s3-blobs.bash
+++ clean-all-references-to-s3-blobs.bash
@@ -2,7 +2,8 @@
 set -x
 set -e # exit on non-zero status
 
-rm .final_builds/license/index.yml
+rm -f .final_builds/license/index.yml
 rm -rf .final_builds/packages
 echo > config/blobs.yml
 rm -rf releases
+
