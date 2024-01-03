a73070d3b6d3d5a306919941c61e31f253f04916
Olivier ORAND <olivier.orand@orange.com>
2024-01-03 17:29:38 +0000

    chore(github-action): create release-<x> branches (fixup)
    only delete branch and disable set -e to allow failure when a branch already exists

--- create-release-branches.bash
+++ create-release-branches.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 #set -x
-set -e # exit on non-zero status
+#set -e # exit on non-zero status
 
 # This script creates new release branches for all available release branches in k3s-io/k3s repo starting from current version in vendir.yml from master branch.
 #
 # When PUSH_OPTIONS=true, it also recreates all existing release branches from master branch
 
 PUSH_OPTIONS=""
-if [ "$FORCE_PUSH" == "true" ] ;then
+if [ "$FORCE_PUSH" = "true" ] ;then
   PUSH_OPTIONS="$PUSH_OPTIONS --force"
 fi
 current_version=$(yq -r '.directories[0].contents[] | select (.path=="k3s-io/k3s") | .githubRelease.tag ' ./vendir.yml)
@@ -29,13 +29,17 @@ for ref in $(listK3sRepoBranches) ; do
     echo "Keeping $v as $v > $MIN_VERSION"
   fi
   continue
-  git branch -d "$BRANCH_NAME"
+  if [ "$FORCE_PUSH" = "true" ] ;then
+    git branch -d "$BRANCH_NAME"
+  fi
   git co -b "$BRANCH_NAME"
   sed -i.orig "s/tag: v.*/tag: v${v}.0/g" vendir.yml
-  ! diff vendir.yml vendir.yml.orig
+  diff vendir.yml vendir.yml.orig
   rm vendir.yml.orig
   git add vendir.yml
   git commit -m "Initial branch creation for k8s version $v"
   git push --set-upstream origin "$BRANCH_NAME" $PUSH_OPTIONS
   git co master
 done
+
+git branch
