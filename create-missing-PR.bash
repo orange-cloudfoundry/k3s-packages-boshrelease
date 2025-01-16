ba0b4a9f9e244a72970787345960ce077490dc06
Olivier ORAND <olivier.orand@orange.com>
2025-01-16 10:31:49 +0100

    chore(github-action): display gh version

--- create-missing-PR.bash
+++ create-missing-PR.bash
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-if ! gh --version 2>&1 >/dev/null;then
+if ! gh --version >/dev/null 2>&1;then
   echo "Please download and install gh cli https://github.com/cli/cli/releases/"
   exit 1
 fi
 
+gh --version
 gh repo set-default orange-cloudfoundry/k3s-packages-boshrelease
 
 for branch in $(git --no-pager branch -r|grep -E "/release-[0-9.]*$");do
--- create-release-branches.bash
+++ create-release-branches.bash
@@ -5,7 +5,7 @@
 # This script creates new release branches for all available release branches in k3s-io/k3s repo starting from current version in vendir.yml from master branch.
 #
 # When PUSH_OPTIONS=true, it also recreates all existing release branches from master branch
-
+gh version
 PUSH_OPTIONS=""
 if [ "$FORCE_PUSH" = "true" ] ;then
   PUSH_OPTIONS="$PUSH_OPTIONS --force"
